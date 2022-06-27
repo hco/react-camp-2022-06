c81ade8b5b83212809b6cb822ceb34bbc84f7451
Hans-Christian Otto <c.otto@suora.com>
2022-06-27 13:32:19 +0200

    Render hello world

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const element = <h1>Hallo Zürich!</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
