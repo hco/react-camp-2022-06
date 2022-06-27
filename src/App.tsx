c6f533809e39c02518938c7953fd31f7df01e44a
Hans-Christian Otto <c.otto@suora.com>
2022-06-27 16:44:54 +0200

    Extracted variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import TimeEntryView from "./components/TimeEntryView";
+import { TimeEntry } from "./domain/TimeEntry";
 
 function App() {
+  const timeEntry: TimeEntry = {
+    comment: "React gelernt",
+    id: "",
+    start: new Date(),
+    end: new Date(),
+  };
+
   return (
     <>
       <h1>Ich bin Christian</h1>
-      <TimeEntryView
-        timeEntry={{
-          comment: "React gelernt",
-          id: "",
-          start: new Date(),
-          end: new Date(),
-        }}
-      />
+      <TimeEntryView timeEntry={timeEntry} />
     </>
   );
 }
