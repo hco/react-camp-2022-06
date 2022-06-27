8acaf66a0501769552d17c5442c10c784687cdc6
Hans-Christian Otto <c.otto@suora.com>
2022-06-27 14:06:56 +0200

    create app component

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Ich bin Christian</h1>;
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
-const element = <h1>Hallo Zürich!</h1>;
+const element = <App />;
 
 ReactDOM.render(element, document.getElementById("root"));
