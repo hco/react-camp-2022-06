a29186dcad937ee3284722e0c3fe3ddebf280444
Hans-Christian Otto <c.otto@suora.com>
2022-06-28 11:06:06 +0200

    Add state to Form

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TimeEntryForm: React.FunctionComponent = () => {
-  console.log("Hallo Konsole!");
+  const [inputValue, setInputValue] = useState("");
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
     console.log("Hello World");
   };
 
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(event.target.value);
+    setInputValue(event.target.value);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <button type="submit">Hinzufügen</button>
+      <input type="text" onChange={handleChange} />
+      {inputValue}
     </form>
   );
 };
