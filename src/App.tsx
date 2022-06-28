9947b59acf17e4d11fc72cafe48814f6cbf38c17
Hans-Christian Otto <c.otto@suora.com>
2022-06-28 10:20:26 +0200

    Add example counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import TimeEntryForm from "./components/TimeEntryForm";
 import TimeEntryView from "./components/TimeEntryView";
 import { TimeEntry } from "./domain/TimeEntry";
+import Counter from "./components/Counter";
 
 function App() {
   const timeEntry: TimeEntry = {
@@ -15,6 +16,7 @@ function App() {
       <h1>Ich bin Christian</h1>
       <TimeEntryView timeEntry={timeEntry} />
       <TimeEntryForm />
+      <Counter />
     </>
   );
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Aktueller Wert: {count}</p>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment!
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
+
+// const NaiveCounter = () => {
+//   let count = 0;
+//
+//   return (
+//     <div>
+//       <p>{count}</p>
+//       <button onClick={() => (count = count + 1)} />
+//     </div>
+//   );
+// };
