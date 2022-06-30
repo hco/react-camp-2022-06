e378cfadf10efaa9e0bc7eff02dcc7c56a96dd90
Hans-Christian Otto <c.otto@suora.com>
2022-06-30 13:56:59 +0200

    implemented first hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
 import TimeEntryForm from "./components/TimeEntryForm";
-import { TimeEntry } from "./domain/TimeEntry";
 import TimeEntriesList from "./components/TimeEntriesList";
-import { useState } from "react";
+import useTimeEntries from "./hooks/useTimeEntries";
 
 function App() {
-  const [timeEntries, setTimeEntries] = useState([
-    {
-      id: "1d5829",
-      comment: "Listen rendern in react gelernt.",
-      start: new Date(),
-      end: new Date(),
-    },
-    {
-      id: "1d5828",
-      comment: "Daten Laden in react gelernt.",
-      start: new Date(),
-      end: new Date(),
-    },
-    {
-      id: "1d5830",
-      comment: "Hooks gelernt.",
-      start: new Date(),
-      end: new Date(),
-    },
-  ]);
-
-  const handleCreateEntry = (timeEntry: TimeEntry) => {
-    setTimeEntries((prevTimeEntries) => [...prevTimeEntries, timeEntry]);
-  };
+  const { timeEntries, addTimeEntry } = useTimeEntries();
 
   return (
     <>
-      <TimeEntryForm onCreateEntry={handleCreateEntry} />
+      <TimeEntryForm onCreateEntry={addTimeEntry} />
       <TimeEntriesList timeEntries={timeEntries} />
     </>
   );
---
+++ src/hooks/useTimeEntries.ts
@@ -0,0 +1,18 @@
+import { TimeEntry } from "../domain/TimeEntry";
+import { useState } from "react";
+
+const useTimeEntries = () => {
+  const [timeEntries, setTimeEntries] = useState<TimeEntry[]>([]);
+
+  return {
+    timeEntries: timeEntries,
+    addTimeEntry: (timeEntry: TimeEntry) => {
+      setTimeEntries((prevTimeEntries) => [...prevTimeEntries, timeEntry]);
+    },
+    removeTimeEntry: (timeEntry: TimeEntry) => {
+      setTimeEntries(timeEntries.filter((t) => t.id !== timeEntry.id));
+    },
+  };
+};
+
+export default useTimeEntries;
