0c267002a88b6c5d9ed761f51add09e4816f5795
Hans-Christian Otto <c.otto@suora.com>
2022-07-20 09:37:43 +0200

    Add Greeting component

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface Props {
+  name: string;
+}
+const Greeting: React.FunctionComponent<Props> = ({ name }) => (
+  <p>Hello, {name}!</p>
+);
+
+export default Greeting;
